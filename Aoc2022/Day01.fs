dea2d1d47d8994d78f3c26c52e186f5a0b400a01
Joonas Sarajärvi <muep@iki.fi>
2022-12-02 19:07:29 +0200

    Reformat day 1 as well

--- Aoc2022/Day01.fs
+++ Aoc2022/Day01.fs
@@ -2,9 +2,12 @@ module Aoc2022.Day01
 
 let splitByEmpty seq =
     let groupId = ref 0
+
     seq
     |> Seq.groupBy (fun txt ->
-        if txt = "" then groupId.Value <- groupId.Value + 1
+        if txt = "" then
+            groupId.Value <- groupId.Value + 1
+
         groupId.Value)
     |> Seq.map (snd >> (Seq.filter (fun t -> 0 < t.Length)))
 
