4a5fce0b7a543224d647d58b6c4356f688f7f965
Joonas Sarajärvi <muep@iki.fi>
2022-12-04 22:50:12 +0200

    Add some kind of a main program

--- Aoc2022/Program.fs
+++ Aoc2022/Program.fs
@@ -1,12 +1,50 @@
-﻿Aoc2022.Day01.part1 "input/day-01.ex"
-Aoc2022.Day01.part1 "input/day-01.txt"
-Aoc2022.Day01.part2 "input/day-01.txt"
-Aoc2022.Day02.part1 "input/day-02.ex"
-Aoc2022.Day02.part1 "input/day-02.txt"
-Aoc2022.Day02.part2 "input/day-02.txt"
-Aoc2022.Day03.part1 "input/day-03.ex"
-Aoc2022.Day03.part1 "input/day-03.txt"
-Aoc2022.Day03.part2 "input/day-03.txt"
-Aoc2022.Day04.part1 "input/day-04.ex"
-Aoc2022.Day04.part1 "input/day-04.txt"
-Aoc2022.Day04.part2 "input/day-04.txt"
+﻿type Solution =
+    { part1: string -> unit
+      part2: string -> unit }
+
+let solutions =
+    [ (1,
+       { part1 = Aoc2022.Day01.part1
+         part2 = Aoc2022.Day01.part2 })
+      (2,
+       { part1 = Aoc2022.Day02.part1
+         part2 = Aoc2022.Day02.part2 })
+      (3,
+       { part1 = Aoc2022.Day03.part1
+         part2 = Aoc2022.Day03.part2 })
+      (4,
+       { part1 = Aoc2022.Day04.part1
+         part2 = Aoc2022.Day04.part2 }) ]
+    |> Map.ofList
+
+let runAll () =
+    Aoc2022.Day01.part1 "input/day-01.ex"
+    Aoc2022.Day01.part1 "input/day-01.txt"
+    Aoc2022.Day01.part2 "input/day-01.txt"
+    Aoc2022.Day02.part1 "input/day-02.ex"
+    Aoc2022.Day02.part1 "input/day-02.txt"
+    Aoc2022.Day02.part2 "input/day-02.txt"
+    Aoc2022.Day03.part1 "input/day-03.ex"
+    Aoc2022.Day03.part1 "input/day-03.txt"
+    Aoc2022.Day03.part2 "input/day-03.txt"
+    Aoc2022.Day04.part1 "input/day-04.ex"
+    Aoc2022.Day04.part1 "input/day-04.txt"
+    Aoc2022.Day04.part2 "input/day-04.txt"
+
+let runDay inputPath day =
+    solutions.[day].part1 inputPath
+    solutions.[day].part2 inputPath
+    ()
+
+let runDayDefault day =
+    runDay $"input/day-%02d{day}.txt" day
+
+[<EntryPoint>]
+let main args =
+    match args with
+    | [| "all" |] -> runAll ()
+    | [| "day"; num |] -> num |> System.Int32.Parse |> runDayDefault
+    | [| "day"; num; inputPath |] -> num |> System.Int32.Parse |> runDay inputPath
+    | _ -> invalidArg $"%A{args}" "Unexpected command line syntax"
+
+    0
