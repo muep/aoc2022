79c64725d042e15566cf16f4a2c8babff0268a1e
Joonas Sarajärvi <muep@iki.fi>
2022-12-06 19:47:48 +0200

    Cleanup

--- Aoc2022/Day06.fs
+++ Aoc2022/Day06.fs
@@ -1,16 +1,17 @@
 module Aoc2022.Day06
 
-let noDupes (chs:char[]) = chs.Length = (chs |> Set.ofSeq |> Set.count) 
+let noDupes (chs: char []) =
+    chs.Length = (chs |> Set.ofSeq |> Set.count)
 
 let part windowsize path =
     System.IO.File.ReadLines path
     |> Seq.head
     |> Seq.windowed windowsize
     |> Seq.zip (Seq.initInfinite (fun a -> a + windowsize))
-    |> Seq.filter (snd >> noDupes) 
+    |> Seq.filter (snd >> noDupes)
     |> Seq.head
     |> fst
-    |> printfn "%A"
-    
+    |> System.Console.WriteLine
+
 let part1 = part 4
 let part2 = part 14
